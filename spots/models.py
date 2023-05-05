516786d28fc138132da00699aeaae0de0427c924
HyeonSeuk <seo5719@naver.com>
2023-05-05 18:15:49 +0900

    project-last

--- spots/models.py
+++ spots/models.py
@@ -5,24 +5,15 @@ from django.utils.safestring import mark_safe
 
 
 class Spot(models.Model):
-    CATEGORY_CHOICES = (
-        ('산', '산'),
-        ('바다', '바다'),
-        ('계곡', '계곡'),
-        ('섬', '섬'),
-    )
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_spots')
     title = models.CharField(max_length=80)
     content = models.TextField(null=False)
     image   = models.ImageField(upload_to='tourlist_destinations/', null=True, blank=True)
-    category   = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
+    category   = models.CharField(max_length=30)
     address = models.CharField(max_length=100)
-    phone_number = models.CharField(max_length=20)
     price_range = models.CharField(max_length=20)
     parking = models.BooleanField(default=False)
-    business_hours = models.CharField(max_length=50)
-    holiday =  models.DateField()
     website = models.URLField(max_length=200, null=True, blank=True)
     latitude = models.FloatField(null=True)
     longitude = models.FloatField(null=True)
